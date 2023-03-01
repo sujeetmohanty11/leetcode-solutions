5dce5e28b42f44b8755507d4ddb6ac284710f724
Sujeet Mohanty <sujeetmohanty11@gmail.com>
2023-03-02 01:17:27 +0530

    Reverse Integer

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -4,10 +4,13 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        if x > 0:
-            return int(str(x)[::-1])
+        if x >= 0:
+            x = int(str(x)[::-1])
         else:
-            return int(str(x)[1:][::-1])*-1
+            x = int(str(x)[1:][::-1])*-1
+
+        return x if -2**31 <= x < 2**31 else 0
+
 
 obj = Solution()
 print(obj.reverse(-120))
